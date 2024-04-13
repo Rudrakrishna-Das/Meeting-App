dcb34671f59a5ffaa140d7349cb62fcc324bcf31
Rudrakrishna-Das <rudradas16996@gmail.com>
2024-04-13 21:39:45 +0530

    meeting setup done

--- app/(root)/meeting/[id]/page.jsx
+++ app/(root)/meeting/[id]/page.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+"use client";
+import Loader from "@/components/Loader";
+import MeetingRoom from "@/components/MeetingRoom";
+import MeetingSetup from "@/components/MeetingSetup";
+import { useGetCallById } from "@/hooks/useGetCallById";
+import { useUser } from "@clerk/nextjs";
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import React, { useState } from "react";
 
-const MeatingId = ({ params }) => {
-  return <div>Metting Room: #{params.id}</div>;
+const MeatingId = ({ params: { id } }) => {
+  const { user, isLoaded } = useUser();
+  const [isSetupCompleted, setIsSetupCompleted] = useState(false);
+  const { call, isCallLoading } = useGetCallById(id);
+  if (!isLoaded || isCallLoading) return <Loader />;
+  return (
+    <main className="h-screen w-full">
+      <StreamCall call={call}>
+        <StreamTheme>
+          {!isSetupCompleted ? (
+            <MeetingSetup setIsSetupCompleted={setIsSetupCompleted} />
+          ) : (
+            <MeetingRoom />
+          )}
+        </StreamTheme>
+      </StreamCall>
+    </main>
+  );
 };
 
 export default MeatingId;
